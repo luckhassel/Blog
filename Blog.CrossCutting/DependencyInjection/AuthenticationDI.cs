2da1981fb8e45d69d7ef64746245ca70a4d0f703
luckhassel <guilhermeluckfett@gmail.com>
2023-10-19 16:34:43 -0300

    azure ad

--- Blog.CrossCutting/DependencyInjection/AuthenticationDI.cs
+++ Blog.CrossCutting/DependencyInjection/AuthenticationDI.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Web;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
@@ -11,30 +12,33 @@ namespace Blog.CrossCutting.DependencyInjection
     {
         public static void AddAuthentication(this IServiceCollection services, IConfiguration configuration)
         {
-            var applicationSettings = new ApplicationSettings();
-            configuration.GetSection(nameof(ApplicationSettings)).Bind(applicationSettings);
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApi(configuration.GetSection("AzureAd"));
 
-            var encodedKey = Encoding.ASCII.GetBytes(applicationSettings.SecuritySettings.Secret);
+            // var applicationSettings = new ApplicationSettings();
+            // configuration.GetSection(nameof(ApplicationSettings)).Bind(applicationSettings);
 
-            services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(x =>
-            {
-                x.RequireHttpsMetadata = false;
-                x.SaveToken = true;
-                x.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(encodedKey),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                };
-            });
+            // var encodedKey = Encoding.ASCII.GetBytes(applicationSettings.SecuritySettings.Secret);
 
-            services.Configure<ApplicationSettings>(configuration.GetSection(nameof(ApplicationSettings)));
+            // services.AddAuthentication(x =>
+            // {
+            //     x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //     x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            // })
+            // .AddJwtBearer(x =>
+            // {
+            //     x.RequireHttpsMetadata = false;
+            //     x.SaveToken = true;
+            //     x.TokenValidationParameters = new TokenValidationParameters
+            //     {
+            //         ValidateIssuerSigningKey = true,
+            //         IssuerSigningKey = new SymmetricSecurityKey(encodedKey),
+            //         ValidateIssuer = false,
+            //         ValidateAudience = false
+            //     };
+            // });
+
+            // services.Configure<ApplicationSettings>(configuration.GetSection(nameof(ApplicationSettings)));
         }
     }
 }
